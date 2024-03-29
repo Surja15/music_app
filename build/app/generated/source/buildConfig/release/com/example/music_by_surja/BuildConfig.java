532bea3b184ae21e2d6ce0285d23ef019782da98
Surja15 <pokedonsss@gmail.com>
2024-03-30 00:06:24 +0530

    Add build folder

---
+++ build/app/generated/source/buildConfig/release/com/example/music_by_surja/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.music_by_surja;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.music_by_surja";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
